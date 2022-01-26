37382a4a9704ad98a6a348b101127b728aaad274
White Wolf Dot <shinjithpallikkara@gmail.com>
2022-01-26 15:46:17 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const chatCont = document.querySelector('#conv-cont');
+const chatForm = document.querySelector('.chat-form');
+const chatInput = document.querySelector('.chat-input');
+
+chatForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const msgText = chatInput.value;
+  if (!msgText) return;
+
+  appendMessage(msgText);
+  chatInput.value = "";
+});
+function appendMessage(text) {
+
+  const msgHTML = `
+  <div class="conv conv-right">
+  <div class="conv-bubble">
+      <div class="msg-info">
+          <div class="msg-info-name">
+              John
+          </div>
+          <div class="msg-info-time">
+              20:11
+          </div>
+      </div>
+      <div class="msg-text">
+          ${text}
+      </div>
+    </div>
+  </div>
+    `;
+  chatCont.insertAdjacentHTML('beforeend', msgHTML);
+  chatCont.scrollTop += 100;
+}
